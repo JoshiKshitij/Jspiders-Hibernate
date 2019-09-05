250520cb4c3508433e065508addfa1d18e83484c
JoshiKshitij <kkshitij.joshi@gmail.com>
2019-09-05 21:14:54 +0530

    cartoon example

---
+++ SEHM-11/Hibernate_Cartoon/src/com/cartoon/app/Cartoons.java
@@ -0,0 +1,43 @@
+package com.cartoon.app;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cartoons implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int cartoonId;
+	
+	private String cartoonName;
+	private String favChar;
+	
+	public int getCartoonId() {
+		return cartoonId;
+	}
+	public void setCartoonId(int cartoonId) {
+		this.cartoonId = cartoonId;
+	}
+	public String getCartoonName() {
+		return cartoonName;
+	}
+	public void setCartoonName(String cartoonName) {
+		this.cartoonName = cartoonName;
+	}
+	public String getFavChar() {
+		return favChar;
+	}
+	public void setFavChar(String favChar) {
+		this.favChar = favChar;
+	}
+	public Cartoons() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ SEHM-11/Hibernate_Cartoon/src/com/cartoon/app/MainRunner.java
@@ -0,0 +1,48 @@
+package com.cartoon.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class MainRunner {
+
+	public static void main(String[] args) {
+
+		// 1st step is configure the app
+		Configuration cfg = new Configuration();
+		cfg.configure();
+
+		// 2 and step
+		// make a connection
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// create session and do CRUD operations
+		Session session = factory.openSession();
+
+		Transaction transaction = session.beginTransaction();
+
+		Cartoons tomAndJerry = new Cartoons();
+
+		tomAndJerry.setCartoonName(" Tom And Jerry ");
+		tomAndJerry.setFavChar("Tom");
+
+		Cartoons naruto = new Cartoons();
+
+		naruto.setCartoonName(" NAruto ");
+		naruto.setFavChar("kakashi");
+
+		Cartoons onePiece = new Cartoons();
+
+		onePiece.setCartoonName(" one piece ");
+		onePiece.setFavChar("luffy");
+
+		session.save(tomAndJerry);
+		session.save(naruto);
+		session.save(onePiece);
+
+		transaction.commit();
+
+	}
+
+}
