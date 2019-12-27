05ac8bd72b08e0f550d107e74c16f3edb67b32a0
JoshiKshitij <kkshitij.joshi@gmail.com>
2019-12-27 10:27:23 +0530

    basics

---
+++ SEHM-12/HIbernate-Basics2/src/Human.java
@@ -0,0 +1,40 @@
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Human implements Serializable {
+
+	@Id
+	@GeneratedValue
+	(strategy= GenerationType.IDENTITY)
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Human() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
+
---
+++ SEHM-12/HIbernate-Basics2/src/Runner.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Configuration cfg = new Configuration();
+		cfg.configure();
+
+		System.out.println("configuraton done");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		System.out.println("factory done");
+
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		Human ram = new Human();
+		ram.setName("ram");
+		
+		
+		 Serializable pk = session.save(ram);
+		 
+		 Integer xyz = (Integer) pk;
+		 int abz = xyz;
+		 
+		 System.out.println(xyz);
+		 
+		 int pk1 = (int)session.save(ram);
+		 
+		 
+		 
+		transaction.commit();
+		
+		
+		
+
+	}
+
+}
