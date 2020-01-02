209ef6630fdae6145dd00763e847fb304df696f8
JoshiKshitij <kkshitij.joshi@gmail.com>
2020-01-02 11:08:53 +0530

    one to one unidirectional

---
+++ SEHM-12/Hibernate-OneToOne-unidirectional/src/com/onetone/app/Laptop.java
@@ -0,0 +1,58 @@
+package com.onetone.app;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Laptop implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String model ;
+	private String brand;
+	
+	@OneToOne
+	private Student student;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	@Override
+	public String toString() {
+		return "Laptop [id=" + id + ", model=" + model + ", brand=" + brand + "]";
+	}
+	public Laptop() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	
+	
+}
---
+++ SEHM-12/Hibernate-OneToOne-unidirectional/src/com/onetone/app/Runner.java
@@ -0,0 +1,37 @@
+package com.onetone.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import singleton.app.HibernteUtil;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = HibernteUtil.buildSessionFactory();
+		
+		Student narayan = new Student();
+		narayan.setName("narayan");
+		narayan.setBranch("EEE");
+		
+		Laptop dell = new Laptop();
+		dell.setBrand("Dell");
+		dell.setModel("XPS 15");
+		dell.setStudent(narayan);
+		
+		
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		session.save(dell);
+		session.save(narayan);
+		
+		transaction.commit();
+		session.close();
+		factory.close();
+		
+	}
+	
+}
